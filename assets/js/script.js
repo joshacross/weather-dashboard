a44684263a137868f0fc124768fa2c41f314bc15
Killa J <joshuaalexandercross@gmail.com>
2021-07-22 06:18:27 -0500

    debugged console

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,61 +23,64 @@ document.getElementById('time').innerHTML = todaysDate;
 
 
 var getWeather = function() {
-const fetch = require("node-fetch");
-const queryString = require('query-string');
-const moment = require("moment");
+    const fetch = require("node-fetch");
+    const queryString = require('query-string');
+    const moment = require("moment");
 
-// set the Timelines GET endpoint as the target URL
-const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+    // set the Timelines GET endpoint as the target URL
+    const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
 
-// get your key from app.tomorrow.io/development/keys
-const apikey = "NXotJE61HZOY6tbotAEzU6sVRCYmTd7J";
+    // get your key from app.tomorrow.io/development/keys
+    const apikey = "NXotJE61HZOY6tbotAEzU6sVRCYmTd7J";
 
-// pick the location, as a latlong pair
-let location = [40.758, -73.9855];
+    // pick the location, as a latlong pair
+    let location = [40.758, -73.9855];
 
-// list the fields
-const fields = [
-  "precipitationIntensity",
-  "precipitationType",
-  "windSpeed",
-  "windGust",
-  "windDirection",
-  "temperature",
-  "temperatureApparent",
-  "cloudCover",
-  "cloudBase",
-  "cloudCeiling",
-  "weatherCode",
-];
+    // list the fields
+    const fields = [
+    "precipitationIntensity",
+    "precipitationType",
+    "windSpeed",
+    "windGust",
+    "windDirection",
+    "temperature",
+    "temperatureApparent",
+    "cloudCover",
+    "cloudBase",
+    "cloudCeiling",
+    "weatherCode",
+    ];
 
-// choose the unit system, either metric or imperial
-const units = "imperial";
+    // choose the unit system, either metric or imperial
+    const units = "imperial";
 
-// set the timesteps, like "current", "1h" and "1d"
-const timesteps = ["current", "1h", "1d"];
+    // set the timesteps, like "current", "1h" and "1d"
+    const timesteps = ["current", "1h", "1d"];
 
-// configure the time frame up to 6 hours back and 15 days out
-const now = moment.utc();
-const startTime = moment.utc(now).add(0, "minutes").toISOString();
-const endTime = moment.utc(now).add(1, "days").toISOString();
+    // configure the time frame up to 6 hours back and 15 days out
+    const now = moment.utc();
+    const startTime = moment.utc(now).add(0, "minutes").toISOString();
+    const endTime = moment.utc(now).add(1, "days").toISOString();
 
-// specify the timezone, using standard IANA timezone format
-const timezone = "America/New_York";
+    // specify the timezone, using standard IANA timezone format
+    const timezone = "America/New_York";
 
-// request the timelines with all the query string parameters as options
-const getTimelineParameters =  queryString.stringify({
-    apikey,
-    location,
-    fields,
-    units,
-    timesteps,
-    startTime,
-    endTime,
-    timezone,
-}, {arrayFormat: "comma"});
+    // request the timelines with all the query string parameters as options
+    const getTimelineParameters =  queryString.stringify({
+        apikey,
+        location,
+        fields,
+        units,
+        timesteps,
+        startTime,
+        endTime,
+        timezone,
+    }, {arrayFormat: "comma"});
 
-fetch(getTimelineURL + "?" + getTimelineParameters, {method: "GET"})
-  .then((result) => result.json(console.log(result)))
-  .then((json) => console.log(json)
-  .catch((error) => console.error("error: " + err));
+    var timeline = function() {
+        fetch(getTimelineURL + "?" + getTimelineParameters, {method: "GET"})
+            .then((result) => result.json(console.log(result)))
+            .then((json) => console.log(json)
+            .catch((error) => console.error("error: " + err))
+        )};
+};
