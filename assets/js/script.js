66dec65dd18054d11a0e21a3a95e72627ec64339
Killa J <joshuaalexandercross@gmail.com>
2021-07-22 06:27:26 -0500

    resolving eerrors with weather api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 var zipcode = document.getElementById('zipcode');
 var city = document.getElementById('city');
 var weatherContainer = document.getElementById('weatherContainer');
-var today = document.getElementById('today').addEventListener(click, getWeatherToday());
-var week = document.getElementById('week').addEventListener(click, getWeatherForecast());
-var submit = document.getElementById('submit').addEventListener(click, getWeather());
+// var today = document.getElementById('today').addEventListener('click', getWeatherToday());
+// var week = document.getElementById('week').addEventListener('click', getWeatherForecast());
+// var submit = document.getElementById('submit').addEventListener('submit', getWeather());
 //////////////////////////
 
 /////////////////////////
@@ -22,10 +22,10 @@ document.getElementById('time').innerHTML = todaysDate;
 /////////////////////////
 
 
-var getWeather = function() {
-    const fetch = require("node-fetch");
-    const queryString = require('query-string');
-    const moment = require("moment");
+var getWeatherToday = function() {
+    // const fetch = require("node-fetch");
+    // const queryString = require('query-string');
+    // const moment = require("moment");
 
     // set the Timelines GET endpoint as the target URL
     const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
@@ -38,17 +38,17 @@ var getWeather = function() {
 
     // list the fields
     const fields = [
-    "precipitationIntensity",
-    "precipitationType",
-    "windSpeed",
-    "windGust",
-    "windDirection",
-    "temperature",
-    "temperatureApparent",
-    "cloudCover",
-    "cloudBase",
-    "cloudCeiling",
-    "weatherCode",
+        "precipitationIntensity",
+        "precipitationType",
+        "windSpeed",
+        "windGust",
+        "windDirection",
+        "temperature",
+        "temperatureApparent",
+        "cloudCover",
+        "cloudBase",
+        "cloudCeiling",
+        "weatherCode",
     ];
 
     // choose the unit system, either metric or imperial
@@ -58,9 +58,9 @@ var getWeather = function() {
     const timesteps = ["current", "1h", "1d"];
 
     // configure the time frame up to 6 hours back and 15 days out
-    const now = moment.utc();
-    const startTime = moment.utc(now).add(0, "minutes").toISOString();
-    const endTime = moment.utc(now).add(1, "days").toISOString();
+    const now = todaysDate
+    const startTime = time.utc(now).add(0, "minutes").toISOString();
+    const endTime = time.utc(now).add(1, "days").toISOString();
 
     // specify the timezone, using standard IANA timezone format
     const timezone = "America/New_York";
@@ -83,4 +83,7 @@ var getWeather = function() {
             .then((json) => console.log(json)
             .catch((error) => console.error("error: " + err))
         )};
+    timeline();
 };
+
+getWeatherToday();
